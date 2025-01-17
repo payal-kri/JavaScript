ae02855400fb468e6c7023a4c697aa429f91e246
payal-kri <payal8002kumari@gmail.com>
2025-01-17 15:08:33 +0530

    Adding new

---
+++ 0_variables.js
@@ -0,0 +1,24 @@
+//* ====================================
+//* Variables in JavaScript
+//* ====================================
+
+const accountId  = 1558
+let accountEmail = "payalkri082@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+
+//accountId = 2 not allowed
+
+accountEmail = "payal.com"
+accountPassword = "3369"
+accountCity = "Delhi"
+console.log(accountId);
+let accountState;
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStateṇ]);
+
